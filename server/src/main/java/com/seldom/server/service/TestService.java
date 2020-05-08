3a5bf3a59aa3c1a36b38203222623495a0971f73
Zixuan Zhang <zhan7230@umn.edu>
2020-05-08 17:24:59 -0500

    Create public server module

---
+++ server/src/main/java/com/seldom/server/service/TestService.java
@@ -0,0 +1,20 @@
+package com.seldom.server.service;
+
+import com.seldom.server.domain.Test;
+import com.seldom.server.mapper.TestMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class TestService {
+
+    @Resource
+    private TestMapper testMapper;
+
+    public List<Test> list() {
+        return testMapper.list();
+    }
+
+}
