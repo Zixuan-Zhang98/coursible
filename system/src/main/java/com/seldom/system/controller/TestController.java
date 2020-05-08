c028fb51984aa587af1778f6a680b81bececc7b5
Zixuan Zhang <zhan7230@umn.edu>
2020-05-08 17:58:34 -0500

    Integrate server module in system module

--- system/src/main/java/com/seldom/system/controller/TestController.java
+++ system/src/main/java/com/seldom/system/controller/TestController.java
@@ -1,7 +1,7 @@
 package com.seldom.system.controller;
 
-import com.seldom.system.domain.Test;
-import com.seldom.system.service.TestService;
+import com.seldom.server.domain.Test;
+import com.seldom.server.service.TestService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
