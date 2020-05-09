18a74d17a795fde9ef18d892bf04e4b0d410dd5e
Zixuan Zhang <zhan7230@umn.edu>
2020-05-08 22:38:45 -0500

    Demonstrate how to use autogenerated Example class which significantly improves efficiency when querying a single table.

--- server/src/main/java/com/seldom/server/service/TestService.java
+++ server/src/main/java/com/seldom/server/service/TestService.java
@@ -1,6 +1,7 @@
 package com.seldom.server.service;
 
 import com.seldom.server.domain.Test;
+import com.seldom.server.domain.TestExample;
 import com.seldom.server.mapper.TestMapper;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +15,11 @@ public class TestService {
     private TestMapper testMapper;
 
     public List<Test> list() {
-        return testMapper.list();
+        TestExample testExample = new TestExample();
+        testExample.createCriteria().andIdEqualTo("1");
+        testExample.setOrderByClause("id desc");
+        return testMapper.selectByExample(testExample);
+//        return testMapper.selectByExample(null);  // select * from test;
     }
 
 }
