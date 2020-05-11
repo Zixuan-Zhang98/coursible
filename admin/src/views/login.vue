9dbe88243e232f8f1a2fe479e23f9571b8b41572
Zixuan Zhang <zhan7230@umn.edu>
2020-05-11 10:34:11 -0500

    Reformat code

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -18,7 +18,7 @@
                                 <div class="widget-body">
                                     <div class="widget-main">
                                         <h4 class="header blue lighter bigger">
-                                            <i class="ace-icon fa fa-coffee green"></i>
+                                            <i class="ace-icon fa fa-coffee green"/>
                                             Please Enter Your Information
                                         </h4>
 
@@ -28,15 +28,17 @@
                                             <fieldset>
                                                 <label class="block clearfix">
 														<span class="block input-icon input-icon-right">
-															<input type="text" class="form-control" placeholder="Username" />
-															<i class="ace-icon fa fa-user"></i>
+															<input type="text" class="form-control"
+                                                                   placeholder="Username"/>
+															<i class="ace-icon fa fa-user"/>
 														</span>
                                                 </label>
 
                                                 <label class="block clearfix">
 														<span class="block input-icon input-icon-right">
-															<input type="password" class="form-control" placeholder="Password" />
-															<i class="ace-icon fa fa-lock"></i>
+															<input type="password" class="form-control"
+                                                                   placeholder="Password"/>
+															<i class="ace-icon fa fa-lock"/>
 														</span>
                                                 </label>
 
@@ -44,12 +46,13 @@
 
                                                 <div class="clearfix">
                                                     <label class="inline">
-                                                        <input type="checkbox" class="ace" />
+                                                        <input type="checkbox" class="ace"/>
                                                         <span class="lbl"> Remember Me</span>
                                                     </label>
 
-                                                    <button type="button" class="width-35 pull-right btn btn-sm btn-primary">
-                                                        <i class="ace-icon fa fa-key"></i>
+                                                    <button type="button"
+                                                            class="width-35 pull-right btn btn-sm btn-primary">
+                                                        <i class="ace-icon fa fa-key"/>
                                                         <span class="bigger-110">Login</span>
                                                     </button>
                                                 </div>
