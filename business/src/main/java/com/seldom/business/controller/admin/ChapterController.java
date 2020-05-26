32c5a17a9a1d62f590488a6c1430bf3afa2e1f00
Zixuan Zhang <zhan7230@umn.edu>
2020-05-25 23:13:26 -0500

    1. Add dto later, which is used in controller and service layer 2. Add ChapterDto 3. Modify ChapterService so that it returns List<ChapterDto> instead of List<Chapter>

--- business/src/main/java/com/seldom/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/seldom/business/controller/admin/ChapterController.java
@@ -1,6 +1,7 @@
-package com.seldom.business.controller;
+package com.seldom.business.controller.admin;
 
 import com.seldom.server.domain.Chapter;
+import com.seldom.server.dto.ChapterDto;
 import com.seldom.server.service.ChapterService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +10,14 @@ import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
+@RequestMapping("/admin")
 public class ChapterController {
 
     @Resource
     private ChapterService chapterService;
 
     @RequestMapping("/chapter")
-    public List<Chapter> chapter() {
+    public List<ChapterDto> chapter() {
         return chapterService.list();
     }
 
