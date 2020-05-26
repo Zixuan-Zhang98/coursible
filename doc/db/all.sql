45c144c606a991fe68a84ec49df5c2ea4599f29d
Zixuan Zhang <zhan7230@umn.edu>
2020-05-25 22:12:53 -0500

    IDEA automatically reformatted all code

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -1,11 +1,15 @@
 # ---------------------- test ----------------------
 
 drop table if exists `test`;
-create table `test` (
-  `id` char(8) not null default '' comment 'id',
-  `name` varchar(50) comment 'name',
-  primary key (`id`)
-) engine=innodb default charset=utf8mb4 comment='test';
+create table `test`
+(
+    `id`   char(8) not null default '' comment 'id',
+    `name` varchar(50) comment 'name',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4 comment ='test';
 
-insert into `test` (id, name) values (1, 'test1');
-insert into `test` (id, name) values (2, 'test2');
+insert into `test` (id, name)
+values (1, 'test1');
+insert into `test` (id, name)
+values (2, 'test2');
