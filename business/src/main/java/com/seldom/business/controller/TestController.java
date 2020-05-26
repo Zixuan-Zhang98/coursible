e3f4482ff153b6d8c427498a2ab3787802c7d1ff
Zixuan Zhang <zhan7230@umn.edu>
2020-05-25 22:13:25 -0500

    Add business module and its initial startup code

---
+++ business/src/main/java/com/seldom/business/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.seldom.business.controller;
+
+import com.seldom.server.domain.Test;
+import com.seldom.server.service.TestService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+public class TestController {
+
+    @Resource
+    private TestService testService;
+
+    @RequestMapping("/test")
+    public List<Test> test() {
+        return testService.list();
+    }
+
+}
