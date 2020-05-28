f98561660c575fe625d209eab537e5a1aa04582d
Zixuan Zhang <zhan7230@umn.edu>
2020-05-28 00:29:28 -0500

    Add pagination component

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
         <p>
-            <button v-on:click="list()" class="btn btn-white btn-default btn-round">
+            <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
                 <i class="ace-icon fa fa-refresh"></i>
                 Refresh
             </button>
         </p>
+        <pagination ref="pagination" v-bind:list="list"></pagination>
         <table id="simple-table" class="table  table-bordered table-hover">
             <thead>
             <tr>
@@ -88,8 +89,10 @@
 </template>
 
 <script>
+    import Pagination from "../../components/pagination";
     export default {
         name: 'welcome',
+        components: {Pagination},
         data: function() {
           return {
               chapters: []
@@ -99,17 +102,19 @@
             // sidebar激活样式方法一
             // this.$parent.activeSidebar("business-chapter-sidebar"
             let _this = this;
-            _this.list();
+            _this.$refs.pagination.size = 5;
+            _this.list(1);
         },
         methods: {
-            list() {
+            list(page) {
                 let _this = this;
                 _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list', {
-                    page: 1,
-                    size: 1
+                    page: page,
+                    size: _this.$refs.pagination.size,
                 }).then((response) => {
                     console.log("Result from querying chapter table: ", response);
                     _this.chapters = response.data.list;
+                    _this.$refs.pagination.render(page, response.data.total)
                 })
             }
         }
