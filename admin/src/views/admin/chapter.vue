5e01d2b72936ac58d85e2dec32f4f9562302cd41
Zixuan Zhang <zhan7230@umn.edu>
2020-05-29 13:53:56 -0500

    Build toast module with sweetalert2 which enables easy pop-up messages

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -168,6 +168,7 @@
                     if (resp.success) {
                         $("#form-modal").modal("hide");
                         _this.list(1);
+                        toast.success("Saved successfully!");
                     }
                 })
             },
@@ -189,11 +190,7 @@
                             let resp = response.data;
                             if (resp.success) {
                                 _this.list(1);
-                                Swal.fire(
-                                    'Deleted!',
-                                    'Your file has been deleted.',
-                                    'success'
-                                )
+                                toast.success("Deleted!")
                             }
                         })
                     }
