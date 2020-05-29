9107deed8b35486e41f2b877732d9b8f58ae3214
Zixuan Zhang <zhan7230@umn.edu>
2020-05-29 17:08:49 -0500

    1. Add ValidatorUtil 2. Add AOP component: ControllerExceptionHandler (keyword: @ControllerAdvice)

---
+++ server/src/main/java/com/seldom/server/util/ValidatorUtil.java
@@ -0,0 +1,32 @@
+package com.seldom.server.util;
+
+import com.seldom.server.exception.ValidatorException;
+import org.springframework.util.StringUtils;
+
+public class ValidatorUtil {
+
+    /**
+     * 空校验（null or ""）
+     */
+    public static void require(Object str, String fieldName) {
+        if (StringUtils.isEmpty(str)) {
+            throw new ValidatorException(fieldName + " cannot be empty");
+        }
+    }
+
+    /**
+     * 长度校验
+     */
+    public static void length(String str, String fieldName, int min, int max) {
+        if (StringUtils.isEmpty(str)) {
+            return;
+        }
+        int length = 0;
+        if (!StringUtils.isEmpty(str)) {
+            length = str.length();
+        }
+        if (length < min || length > max) {
+            throw new ValidatorException("The length of " + fieldName + " should be between " + min + " ~ " + max + " digits");
+        }
+    }
+}
